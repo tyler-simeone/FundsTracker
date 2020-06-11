c3c27c2f7b4791c6788b79f6253740e0e8e932cd
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-11 11:59:04 -0500

    fixed app import in admin app's urls.py

--- fundstrackerproject/fundstrackerproject/urls.py
+++ fundstrackerproject/fundstrackerproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from fundstrackerproject.fundstrackerapp.models import *
+from fundstrackerapp.models import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
