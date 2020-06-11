e4ea0f1e627f5802b68bf04d9cdb5d4beb8ab0db
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-11 13:00:10 -0500

    added placeholder url patterns for new auth views

--- fundstrackerproject/fundstrackerapp/urls.py
+++ fundstrackerproject/fundstrackerapp/urls.py
@@ -5,4 +5,6 @@ app_name = "fundstrackerapp"
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/register', register, name='register'),
+    # path('logout/', logout_user, name='logout'),
 ]
