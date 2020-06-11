91bb00112e5dc104bbf3a8350b89a70cb7a5d774
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-11 11:33:11 -0500

    Added model for monthly expense

---
+++ fundstrackerproject/fundstrackerapp/models/monthlyexpense.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class MonthlyExpense (models.Model):
+
+    name = models.CharField(max_length=50)
+    total = models.DecimalField(max_digits=None, decimal_places=2)
+    user = models.ManyToOneRel(User, on_delete=models.CASCADE)
