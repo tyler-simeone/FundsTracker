f9e60ac02d281e6f750f5807f59a50350691dafa
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-12 10:57:50 -0500

    added code to find net income in net_income_list view

--- fundstrackerproject/fundstrackerapp/views/netincome/list.py
+++ fundstrackerproject/fundstrackerapp/views/netincome/list.py
@@ -3,6 +3,20 @@ from django.contrib.auth.decorators import login_required
 from fundstrackerapp.models import MonthlyIncome, MonthlyExpense
 
 @login_required
-def net_income(request):
-    
-    all_incomes = MonthlyIncome.objects.filter(user=request.auth.user.id)
+def net_income_list(request):
+    if request.method == 'GET':
+        all_incomes = MonthlyIncome.objects.filter(user=request.auth.user.id)
+        all_expenses = MonthlyExpense.objects.filter(user=request.auth.user.id)
+
+        total_income = 0
+        total_expense = 0
+
+        for income in all_incomes:
+            total_income += income.total
+
+        for expense in all_expenses:
+            total_expense += expense.total
+
+        net_income = total_income - total_expense
+
+        template = ''
