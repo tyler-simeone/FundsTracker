016a946697e6d7acdc5ccebdc6f876bccde67610
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-12 08:33:34 -0500

    added view in views/auth/ for logout, and added url pattern for logout

--- fundstrackerproject/fundstrackerapp/urls.py
+++ fundstrackerproject/fundstrackerapp/urls.py
@@ -6,5 +6,5 @@ app_name = "fundstrackerapp"
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register', register, name='register'),
-    # path('logout/', logout_user, name='logout'),
+    path('logout/', logout_user, name='logout'),
 ]
