5365d4417a57e1cfa5cec223b272603c7a45e7c3
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-12 08:25:24 -0500

    can now register new user

--- fundstrackerproject/fundstrackerapp/views/auth/register.py
+++ fundstrackerproject/fundstrackerapp/views/auth/register.py
@@ -2,22 +2,32 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms import ValidationError
 
 def register(request):
-    
+
     if request.method == 'POST':
-        f = UserCreationForm(request.POST)
+        form_data = request.POST
+        
+        try:
+            if form_data['password'] != form_data['password_confirmation']:
+                raise ValidationError("Password and password confirmation do not match.")
 
-        if f.is_valid():
-            f.save()
-            messages.success(request, 'Account created successfully')
-            return redirect('register')
- 
-        else:
-            f = UserCreationForm()
+            new_user = User.objects.create_user(
+                username=form_data['username'],
+                password=form_data['password']
+            )
 
-    template = 'registration/register.html'    
+            user = authenticate(request, username=form_data['username'], password=form_data['password'])
+            if user is not None:
+                login(request, user)
+                # return redirect(reverse('fundstrackerapp:home'))
+
+        except Exception as e:
+            messages.error(request, f'{type(e)}: {e}')
+
+    template = 'registration/register.html'
+    context = {}
  
-    return render(request, template, {'form': f})
+    return render(request, template, context)
