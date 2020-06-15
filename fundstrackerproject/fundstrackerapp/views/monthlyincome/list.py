62a92a7ead020ea1b887a60c61ff7e5535a12505
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-15 11:53:24 -0500

    added conditional in account list view to differentiate the post requests between new income sources and new expenses

--- fundstrackerproject/fundstrackerapp/views/monthlyincome/list.py
+++ fundstrackerproject/fundstrackerapp/views/monthlyincome/list.py
@@ -23,11 +23,19 @@ def income_list(request):
       
     elif request.method == 'POST':
         form_data = request.POST
+
+        if ("expense" in form_data):
+            new_expense = MonthlyExpense.objects.create(
+                name = form_data['name'],
+                total = form_data['total'],
+                user_id = request.user.id
+            )
         
-        new_income = MonthlyIncome.objects.create(
-            name = form_data['name'],
-            total = form_data['total'],
-            user_id = request.user.id
-        )        
+        else:
+            new_income = MonthlyIncome.objects.create(
+                name = form_data['name'],
+                total = form_data['total'],
+                user_id = request.user.id
+            )        
 
         return redirect(reverse('fundstrackerapp:account'))
