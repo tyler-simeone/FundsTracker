190b2341d601ca69760654836644fec80b620bc0
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-16 09:11:47 -0500

    can now edit a financial goal

--- fundstrackerproject/fundstrackerapp/views/__init__.py
+++ fundstrackerproject/fundstrackerapp/views/__init__.py
@@ -9,4 +9,5 @@ from .monthlyincome.details import income_details
 from .monthlyexpense.form import expense_form, expense_edit_form
 from .monthlyexpense.details import expense_details
 from .goals.list import goal_list
-from .goals.form import goal_form
+from .goals.form import goal_form, goal_edit_form
+from .goals.details import goal_details
