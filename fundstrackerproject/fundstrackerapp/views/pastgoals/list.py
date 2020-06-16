6cffa43622e8a06b47e0739da19d77434a6252a7
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-16 14:03:27 -0500

    still working on logic for past goal exp date

--- fundstrackerproject/fundstrackerapp/views/pastgoals/list.py
+++ fundstrackerproject/fundstrackerapp/views/pastgoals/list.py
@@ -14,24 +14,36 @@ def past_goals_list(request):
         
         # Seeing if the goal's expiration date has been reached
         # (making it a past goal)
+        #   getting created_at month, adding the timeframe int,
+        #   now try to rejoin and parse date it to hopefully
+        #   convert the > 12 nums to new year nums
         for goal in one_month_goals:
             goal_date_str = str(goal.created_at)
-            goal_month = int(goal_date_str.split('-')[1])
+            goal_created_date = goal_date_str.split('-')
+            goal_date_sliced = goal_created_date[0:3:2]
             
-            exp_month = goal_month + goal.timeframe
+            exp_month = int(goal_created_date[1]) + goal.timeframe
+
+            goal_date_sliced.insert(1, str(exp_month))
+            goal_date = '-'.join(goal_date_sliced)
+            
 
             # so I need to come up with a way to convert any exp month
             # that is > 12 into the correct next year's month
             if (exp_month > 12):
+                # will add one year to the goal
+                goal_year = str(int(goal_created_date[0]) + 1)
+
                 # ie. 12 + 3 = 15 exp month, 10 + 6 = 16 exp month
                 new_year_months = exp_month - goal.timeframe
 
+
             current_date = str(datetime.datetime.now())
             current_month = int(current_date.split('-')[1])
-            print(exp_month, current_month)
+            print(goal_created_date, goal_date)
 
-            if (exp_month < current_month):
-                past_one_month_goals.append(goal)
+            # if (exp_month < current_month):
+            #     past_one_month_goals.append(goal)
         
         print(past_one_month_goals)
 
