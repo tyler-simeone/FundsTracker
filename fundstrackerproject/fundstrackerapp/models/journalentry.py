3848994a82cbe5f72c9b64998cc510af596acd28
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-18 17:08:28 -0500

    added journal entry fixtures and added created_at field to model

--- fundstrackerproject/fundstrackerapp/models/journalentry.py
+++ fundstrackerproject/fundstrackerapp/models/journalentry.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class JournalEntry (models.Model):
 
     entry = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
