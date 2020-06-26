e59f87267f6c7b68013f0d7631806db770653aa3
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-25 19:34:40 -0500

    removed unnecessary home view that came with the app

--- fundstrackerproject/fundstrackerapp/views/__init__.py
+++ fundstrackerproject/fundstrackerapp/views/__init__.py
@@ -1,6 +1,5 @@
 from .auth.register import register
 from .auth.logout import logout_user
-from .home import home
 from .netincome.list import net_income_list
 from .netincome.details import net_income_details
 from .monthlyincome.list import income_list
--- fundstrackerproject/fundstrackerapp/views/home.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def home(request):
-    if request.method == 'GET':
-        template = 'home.html'
-        context = {}
-
-        return render(request, template, context)
