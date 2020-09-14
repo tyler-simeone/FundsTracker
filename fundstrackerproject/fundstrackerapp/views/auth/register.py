0aefc5b4c2a17817f2cfd20ab1234bed51ceb10a
Tyler Simeone <tyler.simeone@gmail.com>
2020-09-14 16:46:13 -0500

    registration validation fixed via Django UserCreationForm

--- fundstrackerproject/fundstrackerapp/views/auth/register.py
+++ fundstrackerproject/fundstrackerapp/views/auth/register.py
@@ -1,33 +1,23 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ValidationError
 
 def register(request):
 
     if request.method == 'POST':
-        form_data = request.POST
-        
-        try:
-            if form_data['password'] != form_data['password_confirmation']:
-                raise ValidationError("Password and password confirmation do not match.")
+        f = UserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            messages.success(request, 'Account created successfully')
+            
+            return redirect(reverse('fundstrackerapp:home'))
 
-            new_user = User.objects.create_user(
-                username=form_data['username'],
-                password=form_data['password']
-            )
-
-            user = authenticate(request, username=form_data['username'], password=form_data['password'])
-            if user is not None:
-                login(request, user)
-                return redirect(reverse('fundstrackerapp:home'))
-
-        except Exception as e:
-            messages.error(request, f'{type(e)}: {e}')
+    else:
+        f = UserCreationForm()
 
     template = 'registration/register.html'
-    context = {}
+    context = {'form': f}
  
     return render(request, template, context)
